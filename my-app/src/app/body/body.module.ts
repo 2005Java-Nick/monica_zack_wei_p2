08c2552ac73997dcfa25303b3809fc5f451e3451
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-06-10 21:54:51 -0400

    added a button

--- my-app/src/app/body/body.module.ts
+++ my-app/src/app/body/body.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
+import { BodyComponent } from './body.component';
 
 
 
 @NgModule({
-  declarations: [SearchBarComponent],
+  declarations: [SearchBarComponent, BodyComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    SearchBarComponent
+    SearchBarComponent,
+    BodyComponent
   ]
 })
 export class BodyModule { }
